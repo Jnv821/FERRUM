91c8ee434f545c9710aa315d9f75a1220812292b
Jnv821 <95052283+Jnv821@users.noreply.github.com>
2022-03-01 23:29:45 +0100

    Add files via upload

--- Ferrum-Setup.sh
+++ Ferrum-Setup.sh
@@ -5,8 +5,10 @@
 
 # Update pacakges
 apt update
+# Allow file system permission
+termux-setup-system
 # Install ffmpeg & python
-apt -y install ffmpeg python git termux-api jq
+apt -y install ffmpeg python git termux-api
 
 #--##################################
 #--# Add ffmpeg & Python to path #--#
@@ -14,15 +16,15 @@ apt -y install ffmpeg python git termux-api jq
 
 cd ..
 cd usr/etc/
-echo "export PATH=$PATH:~/data/data/com.termux/files/usr/bin/ffmpeg" >> bash.bashrc
+echo "export PATH=$PATH:~/data/data/com.termux/files/usr/bin/ffmpeg"     >> bash.bashrc
 echo "export PATH=$PATH:~/data/data/com.termux/files/usr/bin/python3.10" >> bash.bashrc
 
 #-- Setup its-pointless repo
 curl -LO https://its-pointless.github.io/setup-pointless-repo.sh
-bash setup-pointless-repo.sh 
+bash setup-pointless-repo.sh -y
 
 #-- Install numpy
-pkg install -y numpy 
+pkg -y install numpy 
 
 #-- install rapidfuzz (v1.9.1 for now)
 pip install rapidfuzz==1.9.1
@@ -30,11 +32,16 @@ pip install rapidfuzz==1.9.1
 #-- Install spotdl
 pip install spotdl
 
-#-- Go back to termux home directory 
-cd 
+#-- Remove setup-pointless-repo
+
+cd ~
 
 #-- Dowmload Ferrum from repo.
 
 ##TODO: Download Ferrum.sh and create config
 
-#git clone 
+git clone 
+
+mkdir -p .shortcuts/tasks/ .shortcuts/icons/
+cp ~/FERRUM/Icons/FERRUM.png .shortcuts/icons/Ferrum.sh.png
+mv ~/FERRUM/Ferrum.sh .shortcuts/tasks/Ferrum.sh
